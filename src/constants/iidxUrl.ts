952827f093f9be50bd06742ca00a1827f5d8cc5f
potakusan <msqkn310@gmail.com>
2026-03-28 21:32:19 +0000

    公式サイトurl修正

--- src/constants/iidxUrl.ts
+++ src/constants/iidxUrl.ts
@@ -1,2 +1,4 @@
+import { latestVersion } from "./latestVersion";
+
 export const iidxUrl =
-  "https://p.eagate.573.jp/game/2dx/33/djdata/score_download.html?style=SP";
+  `https://p.eagate.573.jp/game/2dx/${latestVersion}/djdata/score_download.html?style=SP`;
